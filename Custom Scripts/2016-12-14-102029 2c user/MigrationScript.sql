51e91eff04aae76efb1ad2c5d2fe2ff849379c78
unknown <taylorg@IIS-RTP>
2016-12-14 10:51:09 -0500

    r

--- Custom Scripts/2016-12-14-102029 2c user/MigrationScript.sql
+++
@@ -1,9 +0,0 @@
-/*
-Use this migration script to make data changes only. You must commit any additive schema changes first.
-Schema changes and migration scripts are deployed in the order they're committed.
-*/
-
-update t set Region=r.RegionDescription
-from Territories t
-join Region r
-	on t.RegionID=r.RegionID;
