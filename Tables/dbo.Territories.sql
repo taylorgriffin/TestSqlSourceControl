b4172e73734ce61b870163f43ae0e081cec94267
unknown <taylorg@IIS-RTP>
2016-12-14 14:01:32 -0500

    model changes w/ migration script

--- Tables/dbo.Territories.sql
+++ Tables/dbo.Territories.sql
@@ -2,10 +2,8 @@ CREATE TABLE [dbo].[Territories]
 (
 [TerritoryID] [nvarchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [TerritoryDescription] [nchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-[RegionID] [int] NOT NULL
+[Region] [nchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Territories] ADD CONSTRAINT [PK_Territories] PRIMARY KEY NONCLUSTERED  ([TerritoryID]) ON [PRIMARY]
-GO
-ALTER TABLE [dbo].[Territories] ADD CONSTRAINT [FK_Territories_Region] FOREIGN KEY ([RegionID]) REFERENCES [dbo].[Region] ([RegionID])
 GO
