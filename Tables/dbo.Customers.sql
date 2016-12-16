3b447eda8c9f29b1953181e36e6d356971ea2da7
unknown <taylorg@IIS-RTP>
2016-12-16 10:17:30 -0500

    Adding customers history table.

--- Tables/dbo.Customers.sql
+++ Tables/dbo.Customers.sql
@@ -14,6 +14,74 @@ CREATE TABLE [dbo].[Customers]
 [DOB] [datetime] NULL
 ) ON [PRIMARY]
 GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE TRIGGER [dbo].[Customers_HIST_IUD] ON [dbo].[Customers] FOR INSERT, UPDATE, DELETE 
+AS 
+BEGIN
+    SET  NOCOUNT  ON
+
+    DECLARE @ctx_info INT;
+    SELECT @ctx_info = CAST(CAST(CONTEXT_INFO() AS BINARY(4)) AS INT);
+
+    IF @ctx_info IS NOT NULL
+    BEGIN
+        RETURN;
+    END;
+
+    DECLARE @changetype char(1)
+    DECLARE @crrntdate datetime
+
+    SET  @crrntdate =getdate()
+
+    BEGIN TRY
+
+        -- to tell the type of transaction (insert, update, or delete) inserted - exist for I and U.  deleted exists for U and d.
+
+        select @changetype = case 
+            when (exists (Select 1 from Inserted)) and (not exists (Select 1 from deleted)) then 'I'
+            when (not exists(Select 1 from Inserted)) and (exists (Select 1 from deleted)) then 'D'
+            else 'U'
+        end
+
+        -- get inserted for insert and update -latest version
+        if @changetype in ('I','U') 
+            BEGIN
+                INSERT INTO Customers_HIST 
+                    SELECT @changetype
+                         , @crrntdate
+                         , *
+                      FROM Inserted;
+            END;
+        else
+            BEGIN
+                INSERT INTO Customers_HIST 
+                    SELECT @changetype
+                         , @crrntdate
+                         , *
+                      FROM Deleted;
+            END;
+    END TRY
+    BEGIN CATCH
+        DECLARE 
+            @nvErr NVARCHAR(MAX)    
+
+        SET @nvErr =   'Error Msg : ' + ERROR_MESSAGE() 
+                      + COALESCE(' Error Line : ' + CAST(ERROR_LINE() AS VARCHAR(10)),'')    
+                      + COALESCE('. Proc Name : ' + ERROR_PROCEDURE(),'')   
+
+        IF @@TRANCOUNT > 0
+        BEGIN
+            ROLLBACK  
+        END
+
+        RAISERROR(@nvErr,14,1) 
+    END CATCH
+
+END 
+GO
 ALTER TABLE [dbo].[Customers] ADD CONSTRAINT [PK_Customers] PRIMARY KEY CLUSTERED  ([CustomerID]) ON [PRIMARY]
 GO
 CREATE NONCLUSTERED INDEX [City] ON [dbo].[Customers] ([City]) ON [PRIMARY]
---
+++ Tables/dbo.Customers_HIST.sql
@@ -0,0 +1,21 @@
+CREATE TABLE [dbo].[Customers_HIST]
+(
+[Customers_HIST_PK] [numeric] (18, 0) NOT NULL IDENTITY(1, 1),
+[EVENT_TYPE] [char] (1) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[EVENT_DATE] [datetime] NOT NULL,
+[CustomerID] [nchar] (5) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[CompanyName] [nvarchar] (40) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[ContactName] [nvarchar] (30) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[ContactTitle] [nvarchar] (30) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Address] [nvarchar] (60) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[City] [nvarchar] (15) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Region] [nvarchar] (15) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[PostalCode] [nvarchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Country] [nvarchar] (15) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Phone] [nvarchar] (24) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Fax] [nvarchar] (24) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[DOB] [datetime] NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Customers_HIST] ADD CONSTRAINT [PK_Customers_HIST] PRIMARY KEY CLUSTERED  ([Customers_HIST_PK]) ON [PRIMARY]
+GO
